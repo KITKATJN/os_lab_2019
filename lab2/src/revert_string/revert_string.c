a63ffd959e2305eb2bb28d9fedad5d90dc127e6b
Ilya <lvutinilyha@gmail.com>
2021-02-14 00:04:59 +0300

    done 2 task

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,18 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int i = 0;
+	char a;
+
+	while (str[i] != '\0')
+		i++;
+	int j = 0;
+	while (j < (i / 2))
+	{
+		a = str[i - j - 1];
+		str[i - j - 1] = str[j];
+		str[j] = a;
+		j++;
+	}
 }
 
